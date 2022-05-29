bc9614b09b3aa9c723112f40053ebd72fc2782da
mbg <mbgagliardi@gmail.com>
2022-05-29 02:36:11 -0300

    Entrega Final

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//Declaro la variable
+
+/* let tamanioEdificio;
+let dimension;
+
+
+//typeof
+let apellido = 'juan';
+
+console.log('apellido', typeof apellido);
+apellido = 15;
+console.log('apellido', typeof apellido);
+
+const PI = 3.14;
+console.log('PI', typeof PI);
+
+PI = 2;
+
+
+
+let nombreIngresado = prompt("Ingrese su nombre");
+let saludo = "Hola como estás?";
+alert(saludo + " " + nombreIngresado);
+
+
+let numeroIngresadoUno = +(prompt("Ingrese número 1"));
+alert("Número 1 Ingresado" + numeroIngresadoUno);
+let numeroIngresadoDos = +(prompt("Ingrese número 2"));
+alert("Número 1 Ingresado" + numeroIngresadoDos);
+let suma = numeroIngresadoUno + numeroIngresadoDos;
+alert("La suma da" + " " + suma);
+
+*/
+
+//ciclos
+
+for (i = 0; i <= 3; i++) {
+    //alert("Número" + i)
+    console.log("Número" + i);
+}
+
+//desafío complementario
+
+Pedir un numero haciendo uso del promp y sumarle otro número en cada repetición, realizando una salida pro cada resiltado -
